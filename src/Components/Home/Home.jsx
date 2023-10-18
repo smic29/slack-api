7000111c340674047e85ce7807076a7691b0db7c
smic29 <sibulo1991@gmail.com>
2023-10-18 16:11:24 +0800

    Fixed Channel display in Homepage and added timestamp formatting

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from "react";
 import { useData } from "../../Context/DataProvider"
 import './Home.css'
 import axios from "axios";
-import { API_URL } from "../../Constants/Constants";
+import { API_URL, formatTimestamp } from "../../Constants/Constants";
 
 function Home() {
     const { user, userHeaders } = useData();
     const [ channelData, setChannelData ] = useState([]);
+    const [ selectedChannel, setSelectedChannel ] = useState('');
+
+    const handleChannelSelect = (e) => {
+        setSelectedChannel(e.target.value);
+    }
 
     useEffect(() => {
         const fetchChannels = async () => {
@@ -26,28 +31,49 @@ function Home() {
 
     return (
         <div className="home-container">
-            <h1>Welcome to your home page, {user.name}!</h1>
+            <h1>Welcome to your home page! </h1>
             <fieldset className="information-box">
                 <legend>Information</legend>
+                <p>You are signed in as: {user.data.email}</p>
+                <p>User ID: {user.data.id}</p>
             </fieldset>
             <fieldset>
                 <legend>Channels</legend>
                 {channelData && channelData.length > 0 ? (
-                <>
-                <p>Total number of Channels: {channelData.length}</p>
-                {channelData.map(channel => (
-                    <div key={channel.id}>
-                    <p>Channel: {channel.name}</p>
-                    <span>Channel ID: {channel.id}</span>
-                    <span>Created: {channel.created_at}</span>
-                    <span>Last Update: {channel.updated_at}</span>
-                    </div>
-                ))}
-                </>
-                ):(
-                    <p>No Channels to Display. Create one!</p>
-                )
-                }
+                <div className="channel-list">
+                    <p>You have {channelData.length} Channels: </p>
+                    <select value={selectedChannel} 
+                    onChange={handleChannelSelect}>
+                    <option value='' 
+                    disabled>
+                        Select a channel to display details</option>
+                    {channelData.map((channel) => (
+                        <option key={channel.id} value={channel.id}>
+                            {channel.name}
+                        </option>
+                    ))}
+                    </select>
+                </div>
+                ) : ( 
+                    <p>No Channels to display. Create or Join one.</p>
+                )}
+                <div>
+                    {selectedChannel !== '' && (
+                        <div>
+                            <p>Channel ID: {selectedChannel}</p>
+                            {channelData.map((channel) => {
+                                if (channel.id === parseInt(selectedChannel)) {
+                                    return (
+                                        <div key={channel.id}>
+                                            <p>Created: {formatTimestamp(channel.created_at)}</p>
+                                            <p>Last Update: {formatTimestamp(channel.updated_at)}</p>
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </div>
+                    )}
+                </div>
             </fieldset>
             <fieldset>
                 <legend>Messages</legend>
--- src/Constants/Constants.jsx
+++ src/Constants/Constants.jsx
@@ -1,1 +1,16 @@
 export const API_URL = 'http://206.189.91.54/api/v1'
+
+export function formatTimestamp(timestamp) {
+    const date = new Date(timestamp);
+
+    const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    }
+
+    return date.toLocaleString(undefined, options);
+}
