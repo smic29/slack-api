7e75cb968b65b62a15bf4e33cfbc2de2a55a143d
smic29 <sibulo1991@gmail.com>
2023-11-10 18:21:47 +0800

    Cleaned up DMs.jsx code

--- src/Components/DMs/Dms.jsx
+++ src/Components/DMs/Dms.jsx
@@ -1,38 +1,16 @@
 import './Dms.css'
 import { useData } from '../../Context/DataProvider'
-import { useState, useRef, useEffect } from 'react';
-import TypeBox from '../Textarea';
+import { useState, useEffect } from 'react';
 import { API_URL } from '../../Constants/Constants';
 import axios from 'axios';
 import ChannelMsgBox from '../ChannelMsgBox';
 
 function Dms() {
     const [ selectedDM, setSelectedDM ] = useState('');
-    const [ body, setBody ] = useState('');
-    const { userBase, userHeaders, setIsLoadingMsgs, uniqueSenders } = useData();
+    const { userBase, uniqueSenders } = useData();
 
     const handleDMSelect = (email) => {
         setSelectedDM(email)
-    }
-
-    const handleSend = async() => {
-        try {
-            const url = `${API_URL}/messages`
-            const convertID = userBase.find((user) => selectedDM === user.email);
-            const sendData = {
-                'receiver_id': parseInt(convertID.id),
-                'receiver_class': "User",
-                'body': body,
-            }
-            await axios.post(url, sendData, { headers: userHeaders })
-
-            setIsLoadingMsgs(true)
-            setTimeout(() => {
-                setBody('');
-            }, 1500);
-        } catch(error) {
-            alert(error)
-        }
     }
 
     const IdDisplay = userBase.find((user) => selectedDM === user.email);
@@ -46,16 +24,9 @@ function Dms() {
             :'Direct Messages'}</h1>
             </nav>
             <RenderList handleDMSelect={handleDMSelect} selectedDM={selectedDM}/>
-            {selectedDM !== '' && selectedDM !== 'newMsg' ? (
-            <>
-            <RenderDMBox selectedDM={selectedDM}
-            body={body}
-            setBody={setBody}
-            handleSend={handleSend}/>
-            </>
-            ) : selectedDM === 'newMsg' ?(
-                <RenderNewDM selectedDM={selectedDM} />
-            ): null}
+            {selectedDM !== '' &&  (
+            <RenderDMBox selectedDM={selectedDM}/>
+            )}
         </div>
     )
 }
@@ -182,70 +153,6 @@ function RenderDMBox(props) {
     
     return (
         <ChannelMsgBox messages={messages} selectedDM={selectedDM}/>
-    )
-}
-
-function RenderNewDM(props) {
-    const { userBase, userHeaders } = useData();
-    const { selectedDM } = props;
-    const [ id, setID ] = useState('');
-    const [ body, setBody ] = useState('');
-    const [ canSendMsg, setCanSendMsg ] = useState(false);
-
-    const doesUserExist = () => {
-        if (id !== ''){
-            const user = userBase.find((user) => 
-                parseInt(id) === user.id
-            )
-            if (user) {
-                return user.email
-            }
-        } 
-        return 'Does not Exist'
-    }
-
-    const handleSend = async() => {
-        try {
-            const url = `${API_URL}/messages`
-            const sendData = {
-                'receiver_id': parseInt(id),
-                'receiver_class': "User",
-                'body': body,
-            }
-
-            await axios.post(url, sendData, { headers: userHeaders })
-
-            alert(`Message Sent`)
-            setBody('')
-            setCanSendMsg(false)
-        } catch(error) {
-            alert(error)
-        }
-    }
-
-    return (
-        <div className='dm-searchuser'>
-            <input 
-            type='text'
-            value={id}
-            maxLength={4}
-            onChange={(e) => {
-                const inputValue = e.target.value;
-                if (/^[0-9]*$/.test(inputValue)){
-                    setID(e.target.value)
-                }
-            }} />
-            <span>
-                {doesUserExist()}
-            </span>
-            <button
-            onClick={() => setCanSendMsg(true)}
-            disabled={doesUserExist() === 'Does not Exist'}>
-                Send this User a message</button>
-                <span>{body}</span>
-            {canSendMsg ? <TypeBox body={body} setBody={setBody}
-            handleSend={handleSend}/> : null}
-        </div>
     )
 }
 
